52e022fdd7ebd7209d9285231a6ed4c9afdf1dc9
Chris Kahn <ckahn@users.noreply.github.com>
2015-05-08 16:17:36 -0700

    Fix AssocOptions

--- w3d5/lib/03_associatable.rb
+++ w3d5/lib/03_associatable.rb
@@ -10,40 +10,63 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name 
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    attributes = {
+    defaults = {
       foreign_key: "#{name}_id".to_sym,
       primary_key: :id,
       class_name: "#{name.to_s.singularize.camelcase}"
     }
 
-    attributes.merge!(options)
+    defaults.merge!(options)
 
-    @foreign_key = attributes[:foreign_key]
-    @primary_key = attributes[:primary_key]
-    @class_name  = attributes[:class_name]
+    @foreign_key = defaults[:foreign_key]
+    @primary_key = defaults[:primary_key]
+    @class_name  = defaults[:class_name]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{self_class_name.underscore}_id".to_sym,
+      primary_key: :id,
+      class_name: "#{name.to_s.singularize.camelcase}"
+    }
+
+    defaults.merge!(options)
+
+    @foreign_key = defaults[:foreign_key]
+    @primary_key = defaults[:primary_key]
+    @class_name  = defaults[:class_name]
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions(name, options)
+    define_method(name) do
+      foreign_key = options.send(:foreign_key)
+      model_class = options.send(:model_class)
+      primary_key = options.send(:primary_key)
+      DBConnection.execute(<<-SQL, foreign_key)
+        SELECT
+          *
+        FROM
+          #{table_name}
+        WHERE
+          id = ?
+      SQL
+    end
   end
 
   def has_many(name, options = {})
