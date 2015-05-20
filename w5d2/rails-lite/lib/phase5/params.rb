dc613d4520586ba270f0e0e31d47d5a46057a210
Chris Kahn <ckahn@users.noreply.github.com>
2015-05-20 10:31:24 -0700

    Parse string to produce nested hash

--- w5d2/rails-lite/lib/phase5/params.rb
+++ w5d2/rails-lite/lib/phase5/params.rb
@@ -10,9 +10,12 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = parse_www_encoded_form(req.query_string)
+      @params.merge!(route_params)
     end
 
     def [](key)
+      @params[key.to_s]
     end
 
     def to_s
@@ -22,17 +25,42 @@ module Phase5
     class AttributeNotFoundError < ArgumentError; end;
 
     private
+
+    def build_params(data)
+      params = {}
+      data.each do |pair| 
+        keys = pair.first
+        val  = pair.last
+        current = params
+        keys.each.with_index do |key, idx|
+          if idx == keys.length - 1
+            current[key] = val
+          else
+            current[key] ||= {}
+            current = current[key]
+          end
+        end
+      end
+      params
+    end
+
     # this should return deeply nested hash
     # argument format
     # user[address][street]=main&user[address][zip]=89436
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      data = URI::decode_www_form(www_encoded_form)
+      data.map! do |pair|
+        [parse_key(pair.first), pair.last]
+      end
+      build_params(data)
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
